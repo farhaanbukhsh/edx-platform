3adb78cbfa985c1be5cbda358e766ba292042418
Josue Balandrano Coronel <jbc@rmcomplexity.com>
2019-07-03 17:26:36 -0500

    Update enrollment serializer to add finished courses and grades. Add problem submission history endpoint.

--- lms/envs/test.py
+++ lms/envs/test.py
@@ -597,3 +597,17 @@ PROCTORING_SETTINGS = {}
 ############### Settings for Django Rate limit #####################
 
 RATELIMIT_RATE = '2/m'
+
+COURSE_ENROLLMENT_MODES['test'] = {
+    "id": 8,
+    "slug": u"test",
+    "display_name": u"Test",
+    "min_price": 0
+}
+
+COURSE_ENROLLMENT_MODES['test_mode'] = {
+    "id": 9,
+    "slug": u"test_mode",
+    "display_name": u"Test Mode",
+    "min_price": 0
+}
