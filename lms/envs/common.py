3889b0c6f1ae9ed7db69ff6a5c338240d1176525
Kshitij Sobti <kshitij@sobti.in>
2020-07-23 22:41:33 +0530

    Add test plugin to show custom discussion provider

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -3919,5 +3919,6 @@ MAX_BLOCKS_PER_CONTENT_LIBRARY = 1000
 
 ####################### Settings for discussions #######################
 ENABLED_DISCUSSION_PROVIDERS = [
-    'cs_comments'
+    "cs_comments",
+    "test_app",
 ]
