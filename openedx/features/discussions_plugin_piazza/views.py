18a255b113d12564932a04de015e950211e01ba4
Kshitij Sobti <kshitij@sobti.in>
2020-08-31 16:48:48 +0530

    Embed in iframe

--- openedx/features/discussions_plugin_piazza/views.py
+++ openedx/features/discussions_plugin_piazza/views.py
@@ -6,6 +6,8 @@ from openedx.core.djangoapps.content.course_overviews.models import CourseOvervi
 from openedx.core.djangoapps.plugin_api.views import EdxFragmentView
 from openedx.core.djangolib.markup import HTML
 
+import html
+
 
 class PiazzaDiscussionFragmentView(EdxFragmentView):
     DISCUSSION_APP_PROVIDER = "piazza"
@@ -31,22 +33,25 @@ class PiazzaDiscussionFragmentView(EdxFragmentView):
         resource_link_id = str(course_key.make_usage_key('course', course_key.run))
         user_id = str(request.user.id)
         result_sourcedid = "_".join([resource_link_id, user_id])
+        embed = lti_embed(
+            html_element_id='piazza-discussion-lti-embed',
+            lti_launch_url=self.LTI_LAUNCH_URL,
+            oauth_key=discussion_config.private_config["oauth_key"],
+            oauth_secret=discussion_config.private_config["oauth_secret"],
+            resource_link_id=resource_link_id,
+            user_id=user_id,
+            roles='Student',  # TODO Use API for this when available
+            context_id=course_id,  # TODO Use API for this when available
+            context_title=course.display_name_with_default,
+            context_label=course.display_org_with_default,
+            result_sourcedid=result_sourcedid
+        )
 
         fragment = Fragment(
-            HTML(
-                lti_embed(
-                    html_element_id='piazza-discussion-lti-embed',
-                    lti_launch_url=self.LTI_LAUNCH_URL,
-                    oauth_key=discussion_config.private_config["oauth_key"],
-                    oauth_secret=discussion_config.private_config["oauth_secret"],
-                    resource_link_id=resource_link_id,
-                    user_id=user_id,
-                    roles='Student',  # TODO Use API for this when available
-                    context_id=course_id,  # TODO Use API for this when available
-                    context_title=course.display_name_with_default,
-                    context_label=course.display_org_with_default,
-                    result_sourcedid=result_sourcedid
-                )
+            HTML("""
+            <iframe srcdoc='{}' style="width:100%;height:800px;">
+            </iframe>
+            """.format(embed)
             )
         )
         return fragment
